574f5b8923307a6400ea22778be0ac992821bef9
Michael Shum <m1shum@ucsd.edu>
2018-01-25 12:30:15 -0800

    lab 2 reads DHT too

--- main.py
+++ main.py
@@ -2,25 +2,28 @@ from time import gmtime,strftime
 import time
 import csv
 from SmartMCP3008 import SmartMCP3008
-
-ADC_Channel = 2
-#if self == '__init__': 
+from SmartDHT22  import SmartDHT22
+ADC_Channel = 2 #Channel on ADC chip
+DHT_Pin = 4 #GPIO pin, not HW pin, that receives data
+#if self == '__init__':
 output = []
 smartMcp = SmartMCP3008()
+smartDHT = SmartDHT22(DHT_Pin)
 for i in range(60):
 	#get time
 	strTime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 	#get val
-	val = smartMcp.read(ADC_Channel)
-
+	valLight = smartMcp.read(ADC_Channel)
+        valTemp = smartDHT.get_temp_fahrenheit()
+        valHum = smartDHT.get_humidity()
 	#add to output
-	output.append([strTime, val])
+	output.append([strTime, valLight, valTemp, valHum])
         print(strTime +' '+ str(val))
 	#sleep for a minute
 	time.sleep(60)
 
-with open('readings.csv','wb') as csvfile:
+with open('readings.csv','w') as csvfile:
 	csvWriter = csv.writer(csvfile)
 	for o in output:
 		csvWriter.writerow(o)
@@ -37,13 +40,18 @@ with open('readings.csv','r') as csvfile:
 		data.append(row)
 
 x = [d[0] for d in data]
-y = [d[1] for d in data]
+yLight = [d[1] for d in data]
+yTemp = [d[2] for d in data]
+yHum = [d[3] for d in data]
 x = [datetime.strptime(x,'%Y-%m-%d %H:%M:%S') for d in x]
 
-plt.plot_date(x,y,'b-')
-plt.xlabel('time over 1 hour span')
-plt.ylabel('ADC reading of photoresistor in makerspace lab')
-plt.title('ADC Readings of photoresistor over 1 hour span')
+plt.plot_date(x,yLight,'b-',label='Light')
+plt.plot_date(x,yTemp,'r-',label='Temp C')
+plt.plot_date(x,yHum,'g-',label='Humidity %')
+plt.xlabel('Time over 1 hour span')
+plt.ylabel('Reading of photoresistor circuit and DHT sensor')
+plt.legend()
+plt.title('Readings of photoresistor circuit and DHT sensor over 1 hour span')
 plt.show()
 '''
 
